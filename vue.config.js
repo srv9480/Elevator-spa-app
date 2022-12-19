247ee424d13f867e050bdec1001e85b787eb0dc4
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-12-19 21:40:17 +0300

    ADD картинка лифта, конфиг - пути к файлам

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+import path from 'path'
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, './src'),
+      '@images' : path.resolve(__dirname, './src/assets/images'),
+      '@components' : path.resolve(__dirname, './src/components'),
+      '@styles' : path.resolve(__dirname, './src/styles')
+    },
+  },
 })
