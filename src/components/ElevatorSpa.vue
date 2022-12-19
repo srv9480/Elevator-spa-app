240339d524b448b3f23762af1ada1bff6cdcce39
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-12-20 00:18:33 +0300

    ADD добавлена разметка

--- src/components/ElevatorSpa.vue
+++ src/components/ElevatorSpa.vue
@@ -1,32 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <div id="stop" class="stop"></div>
+        <div class="panel-floors">
+    <button id="call5" @click="elevatorCall(1)">1 этаж</button> 
+    <button id="call4" @click="elevatorCall(2)">2 этаж</button> 
+    <button id="call3" @click="elevatorCall(3)">3 этаж</button> 
+    <button id="call2" @click="elevatorCall(4)">4 этаж</button> 
+    <button id="call1" @click="elevatorCall(5)">5 этаж</button> 
+    </div>
+    <table border="0">
+      <tr><td class="elevator_sh"> </td></tr>
+      <tr><td class="elevator_sh"> </td></tr>
+      <tr><td class="elevator_sh"> </td></tr>
+      <tr><td class="elevator_sh"> </td></tr>
+      <tr><td class="elevator_sh"> </td></tr>
+      <div id="elev" class="elev">
+      </div>
+    </table>
+    <div class="elevator_sh">   
+       <img id="elevator" class="elevator" src="@images/el.png" @transitionend="elevatorStop()"/>
+    </div>
+
   </div>
 </template>
 
